8c75f7e72f0addcb2c758fc2bb4107963f8f6bab
Baptiste Robichon <brobicho@student.le-101.fr>
2018-02-01 14:30:28 +0100

    101 Get_Next_Line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: brobicho <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/01/06 16:53:09 by brobicho     #+#   ##    ##    #+#       */
+/*   Updated: 2018/01/26 10:10:11 by brobicho    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 173
+
+int			get_next_line(int const fd, char **line);
+
+#endif
